a12099e82697a0191982fa260a2a6f6314917c34
Damian Babiarz <babiarz_damian@wp.pl>
2022-10-29 13:51:02 +0200

    feat: add get Pokemon utils

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,16 @@
+import { getOptionsForVote } from '@/utils/getRandomPokemon';
 import { trpc } from '@/utils/trpc';
 
 const Home = () => {
-  const { data, isLoading } = trpc.useQuery(['hello', { text: 'World' }]);
-
-  if (isLoading) return <div>Loading...</div>;
-  if (data) return <div>{data.greeting}</div>;
-
+  const [first, second] = getOptionsForVote();
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokémon is Rounder</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-800">{first}</div>
         <div className="p-8">vs</div>
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-800">{second}</div>
       </div>
     </div>
   );
